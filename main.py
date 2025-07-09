5d14520e04225bea552c22f6a4373e854651ef1b
snyderg13 <georgesny13@gmail.com>
2025-07-09 13:16:17 -0400

    first prompt using gemini

--- main.py
+++ main.py
@@ -1,5 +1,25 @@
+import os
+from dotenv import load_dotenv
+from google import genai
+
 def main():
     print("Hello from python-ai-agent!")
+
+    load_dotenv()
+    api_key = os.environ.get("GEMINI_API_KEY")
+
+    client = genai.Client(api_key=api_key)
+    response = client.models.generate_content(
+        model="gemini-2.0-flash-001", contents="Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum."
+    )
+
+    print(response.text)
+
+    prompt_tokens = response.usage_metadata.prompt_token_count
+    response_tokens = response.usage_metadata.candidates_token_count
+    print(f"Prompt tokens: {prompt_tokens}")
+    print(f"Response tokens: {response_tokens}")
+
 
 
 if __name__ == "__main__":
