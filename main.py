8a1649f14d3daf7a55c211ceaf28d76fc7a5436b
snyderg13 <georgesny13@gmail.com>
2025-07-09 13:26:46 -0400

    use list of msg/prompts

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import os
 import sys
 from dotenv import load_dotenv
 from google import genai
+from google.genai import types
 
 def main():
     if len(sys.argv) != 2:
@@ -10,13 +11,16 @@ def main():
 
     user_prompt = sys.argv[1]
     print(f"User-provided prompt: {user_prompt}")
+    messages = [
+        types.Content(role="user", parts=[types.Part(text=user_prompt)]),
+    ]
 
     load_dotenv()
     api_key = os.environ.get("GEMINI_API_KEY")
 
     client = genai.Client(api_key=api_key)
     response = client.models.generate_content(
-        model="gemini-2.0-flash-001", contents=user_prompt
+        model="gemini-2.0-flash-001", contents=messages
     )
 
     print(response.text)
