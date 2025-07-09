5369e4379cb8cbabc41fcb672f0226b19e7dde9f
snyderg13 <georgesny13@gmail.com>
2025-07-09 13:22:48 -0400

    update script to accept prompt param for gemini model

--- main.py
+++ main.py
@@ -1,16 +1,22 @@
 import os
+import sys
 from dotenv import load_dotenv
 from google import genai
 
 def main():
-    print("Hello from python-ai-agent!")
+    if len(sys.argv) != 2:
+        print("Error: No prompt provided")
+        sys.exit(1)
+
+    user_prompt = sys.argv[1]
+    print(f"User-provided prompt: {user_prompt}")
 
     load_dotenv()
     api_key = os.environ.get("GEMINI_API_KEY")
 
     client = genai.Client(api_key=api_key)
     response = client.models.generate_content(
-        model="gemini-2.0-flash-001", contents="Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum."
+        model="gemini-2.0-flash-001", contents=user_prompt
     )
 
     print(response.text)
