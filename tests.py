807358425962cabfe490c7c8618c43f157691226
snyderg13 <georgesny13@gmail.com>
2025-07-15 18:20:13 -0400

    initial stub of tests for get files info

---
+++ tests.py
@@ -0,0 +1,9 @@
+import unittest
+from functions.get_files_info import get_files_info
+
+class TestGetFilesInfo(unittest.TestCase):
+    def test_calc_dir(self):
+        result = get_files_info("calculator", ".")
+    
+    def test_funcs_dir(self):
+        result = get_files_info("functions", ".")
