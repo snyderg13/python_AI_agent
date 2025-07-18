efdde45eb35bdd79ca0ec4a29b3688d63434c8c8
snyderg13 <georgesny13@gmail.com>
2025-07-18 13:58:25 -0400

    update tests.py with some TDD cases

--- tests.py
+++ tests.py
@@ -5,8 +5,17 @@ class TestGetFilesInfo(unittest.TestCase):
     def test_calc_dir(self):
         result = get_files_info("calculator", ".")
     
+    def test_calc_pkg(self):
+        result = get_files_info("calculator/pkg", ".")
+
     def test_funcs_dir(self):
         result = get_files_info("functions", ".")
+
+    def test_nonexistent_dir(self):
+        result = get_files_info("math_probs", ".")
+
+    def test_empty_dir(self):
+        result = get_files_info("math_probs", ".")
 
 if __name__ == "__main__":
     unittest.main()
